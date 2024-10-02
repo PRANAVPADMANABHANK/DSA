d9e1e31561ecb5eb55437cab0d436bc14f7a34a2
pranav <pranavkkeloth@gmail.com>
2024-10-02 17:59:11 +0530

    updated array manipulation

--- dsa.js
+++ dsa.js
@@ -1,9 +1,22 @@
 let arr = ['apple', 'orange']
 console.log(arr.length)
 arr.unshift("banana")
-// arr.push("avocado")
+arr.push("avocado")
 
 
-for(i=arr.length;i>0;i++){
+for(i=arr.length-1;i>=0;i--){
     console.log(arr[i])
 }      
+
+
+console.log("---------")
+
+
+for (let arr1 of arr){
+    console.log(arr1)
+}
+
+console.log("--------")
+
+
+console.log(arr.sort())
