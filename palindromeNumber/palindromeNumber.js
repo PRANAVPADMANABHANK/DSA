adbac5f4caf2c8721af9a7177e5200a7929224bd
Pranav k <pranavkkeloth@gmail.com>
2025-01-02 09:52:16 +0530

    feat: palindrome added

---
+++ palindromeNumber/palindromeNumber.js
@@ -0,0 +1,17 @@
+// const palindromeNumber =(a)=>{
+
+    
+//     a === +a.toString().split('').reverse().join('')? console.log("palindrome"): console.log("not palindrome")
+   
+// }
+
+
+// palindromeNumber(124)
+
+
+// const str = 121
+
+// console.log(str.toString().split("")
+
+
+console.log(2===2)
