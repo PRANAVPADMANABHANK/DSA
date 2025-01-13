768eb1ba54da9ca3044d390c7d8261ddeef07170
Pranav k <pranavkkeloth@gmail.com>
2025-01-13 12:01:47 +0530

    feat: fibonacci number up

---
+++ fibonacciNumber/fibonacciNumber.js
@@ -0,0 +1,24 @@
+// fibonacci number : 0,1,1,2,3,5,8,13,21,...
+
+
+// const fibonacci=(n)=>{
+//     let arr = [0, 1]
+
+    
+//     for(i=2;i<=n;i++){
+//         arr.push(arr[i-2]+arr[i-1])
+//     }
+
+    
+//     console.log(arr)
+
+    
+// }
+
+
+// fibonacci(10)
+
+
+let arr= [0,1]
+arr.unshift(0)
+console.log(arr)
